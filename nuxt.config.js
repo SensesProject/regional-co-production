717d1e3ed18d1d02e1527304acb607c71e86b62b
z3to <6gqrbj@posteo.org>
2020-11-10 16:16:09 +0100

    New header

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,12 @@
+const { getHead } = require('library/src/assets/js/head.js')
 
 export default {
   mode: 'spa',
-  head: {
+  head: getHead({
     title: 'Learn Module of co-production techniques',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'In this module, we illustrate how to use co-production techniques to start or develop adaptation planning given different stages of the policy cycle.' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
+    description: 'In this module, we illustrate how to use co-production techniques to start or develop adaptation planning given different stages of the policy cycle.',
+    image: 'https://climatescenarios.org/share/og-co-production.jpg'
+  }),
   loading: { color: '#fff' },
   css: [
     '@/assets/style/base.scss'
